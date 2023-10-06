66f0d1ce669e552b03c808d3f93edc33fc2d18c5
Gesley de Oliveira Rosa <127444907+GesleyOliveira@users.noreply.github.com>
2023-10-05 22:04:06 -0300

    att

--- ListaExercicios/Lista1.js
+++ ListaExercicios/Lista1.js
@@ -3,8 +3,7 @@ function Ex01() {
     var leitor = require("readline-sync");
     var nome = leitor.question("Digite o seu nome: ");
     var nomeDividido = nome.split(" ");
-    var i = 0;
-    for (; i < nomeDividido.length; i++) {
+    for (var i = 0; i < nomeDividido.length; i++) {
         console.log(nomeDividido[i]);
     }
 }
@@ -23,4 +22,50 @@ function Ex03() {
     var conversor = idade * 365;
     console.log("Voc\u00EA j\u00E1 viveu ".concat(conversor, " dias"));
 }
-Ex03();
+function Ex04() {
+    var leitor = require("readline-sync");
+    var numero = leitor.question("Entre com um numero: ");
+    console.log("N\u00FAmero ".concat(numero, ", antecessores: ").concat(numero - 1, ", ").concat(numero - 2, ", e sucessores: ").concat(Number(numero) + 1, ", ").concat(Number(numero) + 2));
+}
+function Ex05() {
+    var leitor = require("readline-sync");
+    var salario = leitor.question("Entre com o seu salário: ");
+    var ajuste = leitor.question("Entre com o percentual de ajuste: ");
+    var calculo = salario * (Number(ajuste / 100) + 1);
+    console.log("O salario ".concat(salario, " com percentual ").concat(ajuste, ", ficar\u00E1 R$ ").concat(calculo, ",00"));
+}
+function Ex06() {
+    var leitor = require("readline-sync");
+    var dolar = leitor.question("Entre com um numero real: ");
+    var conversao = (dolar * 5.47).toFixed(2);
+    console.log("O valor $ ".concat(dolar, " em reais \u00E9 R$ ").concat((conversao)));
+}
+function Ex07() {
+    var leitor = require("readline-sync");
+    var a = leitor.question("Entre com o primeiro numero: ");
+    var b = leitor.question("Entre com o segundo numero: ");
+    var c;
+    c = a;
+    a = b;
+    b = c;
+    console.log(a, b);
+}
+function Ex08() {
+    var leitor = require("readline-sync");
+    var valorCombustivel = leitor.question("Entre com o valor do combustivel: ");
+    var abastecer = leitor.question("Entre com o valor que ira abastecer: ");
+    var conversor = abastecer / valorCombustivel;
+    console.log("O valor R$ ".concat(abastecer, " sera possivel abastecer ").concat(conversor, " litros"));
+}
+function Ex09() {
+    var leitor = require("readline-sync");
+    var kmInicial = Number(leitor.question("Entre com o km inicial: "));
+    var kmFinal = Number(leitor.question("Entre com o km final: "));
+    var diferenca = kmFinal - kmInicial;
+    var rendimento = 12;
+    var consumoLitros = (diferenca / rendimento).toFixed(2);
+    console.log("O volume utilizado no percurso foi de ".concat(consumoLitros, " litros"));
+}
+function Ex10() {
+}
+Ex09();
--- ListaExercicios/Lista1.ts
+++ ListaExercicios/Lista1.ts
@@ -4,8 +4,7 @@ function Ex01(){
     var leitor = require("readline-sync");
     let nome:String = leitor.question("Digite o seu nome: ");
     let nomeDividido  = nome.split(" ");
-    let i:number = 0
-    for(; i < nomeDividido.length; i++){
+    for(let i=0; i < nomeDividido.length; i++){
         console.log(nomeDividido[i]);
         }
 }
@@ -31,9 +30,59 @@ function Ex03(){
 
 function Ex04(){
     var leitor = require("readline-sync");
-    let numero:number = leitor.require("Entre com um número: ");
-    let antes = 
+    let numero:number = leitor.question("Entre com um numero: ");
+    console.log(`Número ${numero}, antecessores: ${numero-1}, ${numero-2}, e sucessores: ${Number(numero) + 1}, ${Number(numero) + 2}`)
 }
-Ex03()
+
+function Ex05(){
+    var leitor = require("readline-sync");
+    let salario:number = leitor.question("Entre com o seu salário: ");
+    let ajuste:number = leitor.question("Entre com o percentual de ajuste: ");
+    let calculo = salario * (Number(ajuste / 100) + 1);
+    console.log(`O salario ${salario} com percentual ${ajuste}, ficará R$ ${calculo},00`)
+}
+
+function Ex06(){
+    var leitor = require("readline-sync");
+    let dolar = leitor.question("Entre com um numero real: ");
+    let conversao = (dolar * 5.47).toFixed(2);
+    console.log(`O valor $ ${dolar} em reais é R$ ${(conversao)}`)
+}
+
+function Ex07(){
+    var leitor = require("readline-sync");
+    let a = leitor.question("Entre com o primeiro numero: ");
+    let b = leitor.question("Entre com o segundo numero: ");
+    let c:number;
+    c = a;
+    a = b;
+    b = c;
+    console.log(a, b);
+    
+}
+
+function Ex08(){
+    var leitor = require("readline-sync");
+    let valorCombustivel = leitor.question("Entre com o valor do combustivel: ");
+    let abastecer = leitor.question("Entre com o valor que ira abastecer: ");
+    let conversor = abastecer / valorCombustivel;
+    console.log(`O valor R$ ${abastecer} sera possivel abastecer ${conversor} litros`);
+
+}
+
+function Ex09(){
+    var leitor = require("readline-sync");
+    let kmInicial:number = Number(leitor.question("Entre com o km inicial: "));
+    let kmFinal:number = Number(leitor.question("Entre com o km final: "));
+    let diferenca = kmFinal - kmInicial;
+    let rendimento = 12
+    let consumoLitros = (diferenca / rendimento).toFixed(2);
+    console.log(`O volume utilizado no percurso foi de ${consumoLitros} litros`)
+}
+
+function Ex10(){
+
+}
+Ex09()
 
     
