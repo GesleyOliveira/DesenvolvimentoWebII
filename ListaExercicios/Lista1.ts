1994f67268da4945725ce1bfcfb02a554a40a766
geeh.oliveira@hotmail.com <geeh.oliveira@hotmail.com>
2023-10-17 17:45:23 -0300

    att

--- ListaExercicios/Lista1.ts
+++ ListaExercicios/Lista1.ts
@@ -128,6 +128,253 @@ function Ex13(){
     
 }
 
-Ex13();
+function Ex14(){
+    var leitor = require("readline-sync");
+    let temp = Number(leitor.question("Entre com a temperatura °C: "));
+    if(temp <= 18){
+        console.log("O clima está frio!");
+    } else if(19 <= temp && temp <= 23){
+        console.log("O clima é agradável!");
+    } else if(24 <= temp && temp <= 35){
+        console.log("O clima é agradável!");
+    } else if(35 < temp){
+        console.log("O clima é muito quente!");
+    } else {
+        console.log("Entre com um valor correto em °C!");
+    }
+}
+
+function Ex15(){
+    var leitor = require("readline-sync");
+    let quarto = Number(leitor.question("Entre com o tipo do quarto (1.Suite, 2.Suite Master, 3.Suite Mega Blaster): "));
+    let horasOuDias = (leitor.question("Entre com a permanencia, sendo H para horas ou D para dias: ")).toUpperCase();
+    let quantidade = Number(leitor.question("Entre com a quantidade de acordo com horas ou dias: "));
+    if(quarto == 1){
+        let precoHoraPernoite = 30/6;
+        let precoHoraDia = 60/24;
+        let precoDia = 60;
+
+        if(horasOuDias == "H"){
+            if(quantidade <= 6){
+                let calculo = precoHoraPernoite * quantidade;
+                console.log(`Valor a pagar pelo quarto Suite, pela permanencia de ${quantidade} horas, e de R$${calculo}!`);
+            } else if(6 < quantidade){
+                let calculo = precoHoraDia * quantidade;
+                console.log(`Valor a pagar pelo quarto Suite, pela permanencia de ${quantidade} horas, e de R$${calculo}!`);
+            }
+        } else if(horasOuDias == "D"){
+            let calculo = precoDia * quantidade;
+            console.log(`Valor a pagar pelo quarto Suite, pela permanencia de ${quantidade} dias, e de R$${calculo}!`);
+        } else {
+            console.log(`Letra ${horasOuDias} incorreto!`);
+        }
+    } else if(quarto == 2){
+        let precoHoraPernoite = 50/6;
+        let precoHoraDia = 90/24;
+        let precoDia = 90;
+
+        if(horasOuDias == "H"){
+            if(quantidade <= 6){
+                let calculo = precoHoraPernoite * quantidade;
+                console.log(`Valor a pagar pelo quarto Suite, pela permanencia de ${quantidade} horas, e de R$${calculo}!`);
+            } else if(6 < quantidade){
+                let calculo = precoHoraDia * quantidade;
+                console.log(`Valor a pagar pelo quarto Suite, pela permanencia de ${quantidade} horas, e de R$${calculo}!`);
+            }
+        } else if(horasOuDias == "D"){
+            let calculo = precoDia * quantidade;
+            console.log(`Valor a pagar pelo quarto Suite, pela permanencia de ${quantidade} dias, e de R$${calculo}!`);
+        } else {
+            console.log(`Letra ${horasOuDias} incorreto!`);
+        }
+    } else if(quarto == 3){
+        let precoHoraPernoite = 140/6;
+        let precoHoraDia = 140/24;
+        let precoDia = 140;
+
+        if(horasOuDias == "H"){
+            if(quantidade <= 6){
+                let calculo = precoHoraPernoite * quantidade;
+                console.log(`Valor a pagar pelo quarto Suite, pela permanencia de ${quantidade} horas, e de R$${calculo}!`);
+            } else if(6 < quantidade){
+                let calculo = precoHoraDia * quantidade;
+                console.log(`Valor a pagar pelo quarto Suite, pela permanencia de ${quantidade} horas, e de R$${calculo}!`);
+            }
+        } else if(horasOuDias == "D"){
+            let calculo = precoDia * quantidade;
+            console.log(`Valor a pagar pelo quarto Suite, pela permanencia de ${quantidade} dias, e de R$${calculo}!`);
+        } else {
+            console.log(`Letra ${horasOuDias} incorreto!`);
+        }
+    } else {
+        console.log(`Valor ${quarto} de quarto incorreto!`);
+    }
+}
+
+function Ex16(){
+    var leitor = require("readline-sync");
+    let diaSemana = Number(leitor.question("Entre com um dia da semana (1 a 7): "));
+    let diaDaSemana = " ";
+
+    switch (diaSemana) {
+        case 1:
+          diaDaSemana = "Domingo";
+          break;
+        case 2:
+          diaDaSemana = "Segunda-feira";
+          break;
+        case 3:
+          diaDaSemana = "Terça-feira";
+          break;
+        case 4:
+          diaDaSemana = "Quarta-feira";
+          break;
+        case 5:
+          diaDaSemana = "Quinta-feira";
+          break;
+        case 6:
+          diaDaSemana = "Sexta-feira";
+          break;
+        case 7:
+          diaDaSemana = "Sábado";
+          break;
+        default:
+          diaDaSemana = "Número fora do intervalo (1 a 7)";
+          break;
+      }
+    
+      console.log(`O dia da semana correspondente a entrada ${diaSemana} é ${diaDaSemana}`);
+}
+
+function Ex17(){
+    var leitor = require("readline-sync");
+    let regiao = (leitor.question("Entre com a regiao do Brasil: ")).toUpperCase();
+    let regioes = [];
+    switch (regiao){
+        case "NORTE":
+            regioes = ["Amazonas", "Roraima", "Amapa", "Para", "Tocantins", "Rondônia", "Acre"];
+            break;
+        case "NORDESTE":
+            regioes = ["Maranhão", "Piauí", "Ceará", "Rio Grande do Norte", "Pernambuco", "Paraíba", "Sergipe", "Alagoas", "Bahia"];
+            break;
+        case "CENTRO-OESTE":
+            regioes = ["Mato Grosso", "Mato Grosso do Sul", "Goiás"];
+            break;
+        case "SUDESTE":
+            regioes = ["São Paulo", "Rio de Janeiro", "Espírito Santo", "Minas Gerais"];
+            break;
+        case "SUL":  
+            regioes = ["Paraná", "Rio Grande do Sul", "Santa Catarina"];
+            break;
+        default:
+            regioes = ["Entre com uma regiao valida!"];
+            break;
+    }
+    console.log(`Estados: ${regioes}`);
+}
+
+function Ex18(){
+    var leitor = require("readline-sync");
+    const senhaSistemaGerente1 = 123456;
+    const nomeSistemaGerente1 = "Fulaninho";
+    const senhaSistemaGerente2 = 654321;
+    const nomeSistemaGerente2 = "Ciclaninho";
+    let codigoProduto = Number(leitor.question("Entre com o codigo do produto: "));
+    let nomeGerente1 = leitor.question("Entre com o nome do Gerente 1: ");
+    let senhaGerente1 = Number(leitor.question("Entre com a senha do Gerente 1: "));
+    let nomeGerente2 = leitor.question("Entre com o nome do Gerente 2: ");
+    let senhaGerente2 = Number(leitor.question("Entre com a senha do Gerente 2: "));
+    if(nomeGerente1 == nomeSistemaGerente1 && nomeGerente2 == nomeSistemaGerente2){
+        if(senhaGerente1 == senhaSistemaGerente1 && senhaGerente2 == senhaSistemaGerente2){
+            console.log(`Cancelamento realizado para o produto código ${codigoProduto}!`);
+        } else {
+            console.log("Senha incorreta!");
+        }
+    } else {
+        console.log("Credencial nome incorreto!");
+    }
+}
+
+function Ex19(){
+    var leitor = require("readline-sync");
+    let operacao = Number(leitor.question("Entre com a operacao desejada (1 - SAQUE, 2 - DEPOSITO, 3 - SALDO): "));
+    let saldo = 500;
+    switch(operacao){
+        case 1:
+            let valorSaque = leitor.question("Entre com o valor do saque: ")
+            if(valorSaque <= saldo){
+                let novoSaldo = saldo - valorSaque;
+                console.log(`Saque realizado! Novo saldo R$ ${novoSaldo}`);
+            } else if(saldo < valorSaque){
+                console.log("Saldo insuficiente!");
+            } else {
+                console.log("Entre com um valor válido!");
+            }
+            break;
+        case 2:
+            let valorDeposito = Number(leitor.question("Entre com o valor do deposito: "));
+            if(0 < valorDeposito){
+                let novoSaldo = saldo + valorDeposito;
+                console.log(`Deposito realizado! Novo saldo R$ ${novoSaldo}`);
+            } else if(0 == valorDeposito){
+                console.log("Entre com um valor maior do que zero!");
+            } else {
+                console.log("Entre com um valor válido!");
+            }
+            break;
+        case 3:
+            console.log(`Saldo atual de ${saldo}. Agradecemos por usar os nossos servicos!`);
+            break;
+        default:
+            console.log("Valor de operação inválido!");
+    }
+}
+
+function Ex20(){
+    var leitor = require("readline-sync");
+    var contador = 0;
+    let soma = 0;
+    while (contador < 10){
+        let numero = Number(leitor.question("Entre com um número"));
+        soma += numero;
+        contador ++;
+    }
+    console.log(`A soma dos valores é ${soma}`);
+}
+
+function Ex21(){
+    var leitor = require("readline-sync");
+    var i = 0;
+    var soma = 0;
+    var media = 0;
+    do {
+        let numero = Number(leitor.question("Entre com um número"));
+        soma += numero;
+        media = numero / 10;
+        i+= 1;
+    } while (i<10);
+    console.log(`A media dos valores é ${media}`);
+}
+
+function Ex22(){
+    var leitor = require("readline-sync");
+    var multiplicacao = 0;
+    for (var i = 0; i<10; i++){
+        let numero = Number(leitor.question("Entre com um número"));
+        multiplicacao *= numero;
+    }
+    console.log(`A media dos valores é ${multiplicacao}`);
+}
+
+function Ex23(){
+    
+}
+
+
+
+
+
+
+Ex19();
 
     
